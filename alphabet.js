8755e4f8635d16c10960e83efa7838feb20f800b
Sofiane Benhamed <sofianebenhamed21@gmail.com>
2022-12-08 13:18:25 +0100

    Ajout de alphabet.js

---
+++ alphabet.js
@@ -0,0 +1,11 @@
+const alphabet = ("abcdefghijklmnopqrstuvwxyz");
+var abc = "";
+
+// Put alphabet into abc: 
+for(i = 0; i < alphabet.length; i++){
+    
+    abc += alphabet.charAt(i);
+}
+
+
+console.log(abc);
