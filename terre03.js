70b6ea12052708da12e28fa78f2c9cdf20127b2f
Sofiane Benhamed <sofianebenhamed21@gmail.com>
2022-12-24 21:30:59 +0100

    modif terre03.js

--- terre03.js
+++ terre03.js
@@ -1,12 +1,16 @@
 const abc = "abcdefghijklmnopqrstuvwxyz";
-const letter = process.argv[2].toLowerCase();
 
-let indexLetter = abc.indexOf(letter);
-let newAbc = abc.charAt(indexLetter);
+// Verify if input is a letter:
 
-for(i = indexLetter + 1; i < abc.length; i++){
-     
-    newAbc += abc.charAt(i);
+if(process.argv[2] && isNaN(process.argv[2])){
+    
+    const inputLetter = process.argv[2].toLowerCase();
+    let indexLetter = abc.indexOf(inputLetter);
+    let newString = abc.charAt(indexLetter);
+
+    for(i = indexLetter + 1; i < abc.length; i++){    
+        newAbc += abc.charAt(i);
+    }
+
+    console.log(newString);
 }
-
-console.log(newAbc);
